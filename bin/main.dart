5a6cff28885a8428320389a2784781bb3abcc333
Vaalls <bielvalls4@gmail.com>
2024-10-10 16:26:01 -0300

    Criando os cabeçalhos da requisição. Enviando um dado através do post.

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,4 @@
+import 'package:dart_assincronismo/api.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 
@@ -44,8 +45,21 @@ sendDataAsync(Map<String, dynamic> mapAccount) async{
   List<dynamic> listAccount = await resquestDataAsync();
   listAccount.add(mapAccount);
   String content = jsonEncode(listAccount);
-  String url ="https://gist.githubusercontent.com/Vaalls/7c1c19cba87f199ab0cbe3bfcd87f1b0/raw/5fdd0d7c1ec75380c8a0bdb6c60cbcd8aa51f953/accounts.json";
 
-  Response response = await post(Uri.parse(url), body: content);
+  String url ="https://api.github.com/gists/7c1c19cba87f199ab0cbe3bfcd87f1b0";
+
+  Response response = await post(
+    Uri.parse(url),
+    headers: {"Authorization": "Bearer $gitHubdApiKey"},
+    body: json.encode({
+      "description": "account.json",
+      "public": true,
+      "files": {
+        "accounts.json": {
+          "content": content,
+        }
+      }
+    }),
+  );
   print(response.statusCode);
 }

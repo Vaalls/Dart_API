34f798e8e1a4c7493fe8586818efd8ae08270dfd
Vaalls <bielvalls4@gmail.com>
2024-10-10 13:46:26 -0300

    Diferenciando Async e Future.

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 
 void main(){
   // print("Olá mundo");
-  requestData();
+  //requestData();
+  requestDataAsync();
 }
 
 // Conectando nossa aplicação a web
@@ -13,6 +14,7 @@ requestData(){
   print(futureResponse);
   futureResponse.then(
     (Response response) {
+    //Tudo até linha 23 esta agendado para acontecer, depois que o get for achado...
      print(response);
      print(response.body);
      List<dynamic> listAccounts = json.decode(response.body);
@@ -22,4 +24,14 @@ requestData(){
      print(mapCarla["balance"]);
     },
   );
+
+  print("Ultima coisa a acontecer na aplicação.");
+}
+
+requestDataAsync() async{
+  String url = "https://gist.githubusercontent.com/Vaalls/7c1c19cba87f199ab0cbe3bfcd87f1b0/raw/5fdd0d7c1ec75380c8a0bdb6c60cbcd8aa51f953/accounts.json";
+  Response response = await get(Uri.parse(url));
+  //Só passa para a linha 35 após finalizar o get
+  print(json.decode(response.body)[0]);
+  print("De fato, vai ser a ultima coisa a acontecer na função.");
 }

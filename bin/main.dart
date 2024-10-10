6315fafb67c132bfb1a399bd4b46de448f3c2858
Vaalls <bielvalls4@gmail.com>
2024-10-10 13:20:01 -0300

    Entendendo o Future. O método then() vem do inglês e significa "e então". Assim, quando o Future terminar, poderemos fazer algo efetivamente. Portanto, entre os parênteses de then(), precisamos passar o que queremos que aconteça ao final do Future.

--- bin/main.dart
+++ bin/main.dart
@@ -10,4 +10,10 @@ requestData(){
   String url = "https://gist.githubusercontent.com/Vaalls/7c1c19cba87f199ab0cbe3bfcd87f1b0/raw/5fdd0d7c1ec75380c8a0bdb6c60cbcd8aa51f953/accounts.json";
   Future<Response> futureResponse = get(Uri.parse(url));
   print(futureResponse);
+  futureResponse.then(
+    (Response response) {
+     print(response);
+     print(response.body);
+    },
+  );
 }

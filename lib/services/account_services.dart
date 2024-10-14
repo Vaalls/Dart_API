a477c0e84b825fab5c7f3940f8ec75bd612e374a
Vaalls <bielvalls4@gmail.com>
2024-10-14 16:20:39 -0300

    Utilizando modelo no getAll.

--- lib/services/account_services.dart
+++ lib/services/account_services.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:dart_assincronismo/api.dart';
+import 'package:dart_assincronismo/models/account.dart';
 import 'package:http/http.dart';
 
 class AccountService {
@@ -11,10 +13,23 @@ class AccountService {
       "https://gist.githubusercontent.com/Vaalls/7c1c19cba87f199ab0cbe3bfcd87f1b0/raw/5fdd0d7c1ec75380c8a0bdb6c60cbcd8aa51f953/accounts.json";
 
   // Conectando nossa aplicação a web
-  Future<List<dynamic>> getAll() async {
+  Future<List<Account>> getAll() async {
     Response response = await get(Uri.parse(url));
     _streamController.add("${DateTime.now()} | Requisição de leitura.");
-    return json.decode(response.body);
+
+    Map<String, dynamic> mapResponse = json.decode(response.body);
+    List<dynamic> listDynamic = 
+        json.decode(mapResponse["files"]["accounts.json"]["content"]); 
+    
+    List<Account> listAccounts = [];
+
+    for (dynamic dyn in listDynamic) {
+      Map<String, dynamic> mapAccount = dyn as Map<String, dynamic>;
+      Account account = Account.fromMap(mapAccount);
+      listAccounts.add(account); 
+    }
+
+    return listAccounts;
   }
 
   addAccount(Map<String, dynamic> mapAccount) async {
